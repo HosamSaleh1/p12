fcdaaea271e251e02e5cf238ee441128de889c6a
galal husseny <galal.husseny@gmail.com>
2021-11-07 11:53:48 +0200

    first day

---
+++ 1st/welcome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "esraa"; ?></title>
+</head>
+<body>
+    <style>
+        .blue {
+            color:blue;
+        }
+    </style>
+    
+    <?php
+      echo "hello World";  
+    ?>
+    <p class="<?php echo "blue" ?>">  Hello World <?php echo "blue" ?> </p>
+    <script>
+        let x = 5;
+        console.log(x);
+    </script>
+</body>
+</html>
