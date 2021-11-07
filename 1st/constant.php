bee7ee2f0d693e580bd30d299cebe3116b0ef2ef
galal husseny <galal.husseny@gmail.com>
2021-11-07 15:05:02 +0200

    first day part2

---
+++ 1st/constant.php
@@ -0,0 +1,18 @@
+<?php
+
+$price = 2000;
+
+$price = 2000 - (2000*0.1);
+
+// echo $price;
+
+
+define('perPage',10);
+
+// echo 'perPage';
+// echo $perPage;
+// echo perpage;
+
+
+// perPage = 66;
+
---
+++ 1st/variable.php
@@ -0,0 +1,29 @@
+<?php
+
+$number = 1000498488;
+$fname =  "galal";
+$lname = "Husseny";
+$status = false;
+$activities = ['gym','football'];
+$hobbies = (object)['first'=>'reading','second'=>'running'];
+$code = null;
+$reviews = 2.6;
+
+
+// echo ($hobbies);
+// print($reviews);
+// print_r($reviews);
+// var_dump($code);
+$p = "<h1>Hello World</h1>";
+// echo $p;
+// echo $fname;
+// echo ' ';
+// echo $lname;
+
+
+// $fullname = $fname . ' ' . $lname;
+// $fullname = "$fname $lname";
+// echo $fullname;
+
+// echo $fname . ' ' . $lname . ' and my activites:' . $activities;
+?>
